69fdaea1ca0ab40c3f6120337997581b355957f7
rodlucenac <digolucena14@gmail.com>
2024-05-28 20:48:00 -0300

    update

--- src/main.c
+++ src/main.c
@@ -8,276 +8,137 @@
 #include <time.h>
 #include <string.h>
 
-#define RESET   "\x1b[0m"
-#define AZUL    "\033[1;36m"
-#define VERDE   "\x1b[32m"
-#define AMARELO "\x1b[33m"
-#define LARANJA "\x1b[38;5;208m"
-#define VERMELHO "\x1b[31m"
+#define RESET          "\x1b[0m"
+#define PRETO          "\x1b[30m"
+#define VERMELHO       "\x1b[31m"
+#define VERDE          "\x1b[32m"
+#define AMARELO        "\x1b[33m"
+#define AZUL           "\x1b[34m"
+#define MAGENTA        "\x1b[35m"
+#define CIANO          "\x1b[36m"
+#define BRANCO         "\x1b[37m"
+#define CINZA          "\x1b[90m"
+#define VERMELHO_CLARO "\x1b[91m"
 
-bool comeuComida = false;
-bool iniciarJogo = false;
-bool perdeuJogo = false;
-int velocidadeCobra = 250000;
-int contador = 0;
+typedef struct {
+  int posX;
+  int posY;
+} Alimento;
 
-char comidaChar = '*';
-char cobraChar = 'O';
-char *corCobra = VERDE;
-char *corComida = AMARELO;
+typedef struct Nodo {
+  int posX;
+  int posY;
+  struct Nodo *prox;
+} Nodo;
 
-FILE *fp;
+typedef struct {
+  Nodo *inicio;
+  int dirX;
+  int dirY;
+} Serpente;
 
-typedef struct Segmento {
-  int x;
-  int y;
-  struct Segmento *proximo;
-} Segmento;
-
-typedef struct Cobra {
-  Segmento *cabeca;
-  int dx;
-  int dy;
-} Cobra;
-
-typedef struct Comida {
-  int x;
-  int y;
-} Comida;
-
-typedef struct Ranking {
+typedef struct {
   char nome[50];
   int pontuacao;
 } Ranking;
 
-void moverCobra(Cobra *cobra, int dx, int dy, Comida *comida);
-void gerarComidaEmLocalAleatorio(Comida *comida);
-void exibirJogo(Cobra *cobra, Comida *comida);
-bool colidiu(Cobra *cobra);
-void configuracoes();
-void salvarPontuacao(const char* nomeJogador, int contador);
-void mostrarRanking();
-void menuPrincipal();
+bool comecarJogo = false;
+bool jogoPerdido = false;
+int velocidade = 250000;
+bool comeuAlimento = false;
+int pontos = 0;
 
-int main() {
-  keyboardInit();
-  screenInit(0);
-  char nomeJogador[50];
-  printf("Por favor, insira seu nome: ");
-  fgets(nomeJogador, 50, stdin);
-  nomeJogador[strcspn(nomeJogador, "\n")] = 0;
+char charAlimento = '*';
+char charSerpente = 'O';
+char *corSerpente = VERDE;
+char *corAlimento = AMARELO;
 
-  menuPrincipal();
-
-  while (1) {
-    if (keyhit()) {
-      char tecla = readch();
-      switch (tecla) {
-      case '1':
-        velocidadeCobra = 250000;
-        iniciarJogo = true;
-        break;
-      case '2':
-        velocidadeCobra = 170000;
-        iniciarJogo = true;
-        break;
-      case '3':
-        velocidadeCobra = 140000;
-        iniciarJogo = true;
-        break;
-      case '4':
-        velocidadeCobra = 100000;
-        iniciarJogo = true;
-        break;
-      case '5':
-        velocidadeCobra = 50000;
-        iniciarJogo = true;
-        break;
-      case '6':
-        configuracoes();
-        menuPrincipal();
-        break;
-      case '7':
-        mostrarRanking();
-        menuPrincipal();
-        break;
-      case '8':
-        keyboardDestroy();
-        return 0;
-      }
-    }
-
-    if (iniciarJogo) {
-      srand(time(NULL));
-      screenInit(1);
-
-      Cobra cobra;
-      Comida comida;
-
-      cobra.cabeca = (Segmento*)malloc(sizeof(Segmento));
-      cobra.cabeca->x = 10;
-      cobra.cabeca->y = 10;
-      cobra.cabeca->proximo = NULL;
-
-      cobra.dx = 1; // -1 esquerda, 1 direita
-      cobra.dy = 0; // -1 cima, 1 baixo
-
-      gerarComidaEmLocalAleatorio(&comida);
-
-      while (iniciarJogo) {
-        if (keyhit() && !perdeuJogo) {
-          int tecla = readch();
-          int novo_dx = cobra.dx;
-          int novo_dy = cobra.dy;
-
-          switch (tecla) {
-          case 119: // w
-            novo_dx = 0;
-            novo_dy = -1;
-            break;
-          case 97: // a
-            novo_dx = -1;
-            novo_dy = 0;
-            break;
-          case 115: // s
-            novo_dx = 0;
-            novo_dy = 1;
-            break;
-          case 100: // d
-            novo_dx = 1;
-            novo_dy = 0;
-            break;
-          case 27: // ESC
-            iniciarJogo = false;
-            break;
-          }
-          if (!(novo_dx == -cobra.dx && novo_dy == -cobra.dy)){
-            cobra.dx = novo_dx;
-            cobra.dy = novo_dy;
-          }
-        }
-
-        moverCobra(&cobra, cobra.dx, cobra.dy, &comida);
-        exibirJogo(&cobra, &comida);
-        usleep(velocidadeCobra);
-
-        if (colidiu(&cobra)){
-          perdeuJogo = true;
-          screenInit(0);
-          printf("\n\n Você deseja continuar jogando? (s/n)\n");
-          while (1) {
-            if (keyhit()) {
-              char tecla = readch();
-              if (tecla == 's') {
-                cobra.cabeca = (Segmento *)malloc(sizeof(Segmento));
-                cobra.cabeca->x = 10;
-                cobra.cabeca->y = 10;
-                cobra.cabeca->proximo = NULL;
-
-                cobra.dx = 1;
-                cobra.dy = 0;
-
-                gerarComidaEmLocalAleatorio(&comida);
-                comeuComida = false;
-                perdeuJogo = false;
-
-                contador = 0;
-                break;
-              } else if (tecla == 'n') {
-                perdeuJogo = false;
-                iniciarJogo = false;
-                salvarPontuacao(nomeJogador, contador);
-                menuPrincipal();
-                break;
-              }
-            }
-          }
-        }
-      }
-      screenDestroy();
-    }
-  }
-  keyboardDestroy();
-  return 0;
+void limparTela() {
+  printf("\033[H\033[J");
 }
 
-void menuPrincipal() {
+void mostrarMenu() {
   screenInit(0);
-  printf("Escolha dificuldade: \n");
-  printf("1 - Super fácil\n");
+  printf("SNAKE GAME\n");
+  printf("1 - Muito fácil\n");
   printf("2 - Fácil\n");
   printf("3 - Médio\n");
   printf("4 - Difícil\n");
-  printf("5 - Impossível\n");
-  printf("6 - Configurações\n");
-  printf("7 - Mostrar Ranking\n");
-  printf("8 - Sair\n");
-  printf("Pressione a tecla correspondente.\n");
+  printf("5 - Muito dificil\n");
+  printf("6 - Impossivel\n");
+  printf("7 - Configurações\n");
+  printf("8 - Mostrar Ranking\n");
+  printf("9 - Sair\n");
 }
 
-void moverCobra(Cobra *cobra, int dx, int dy, Comida *comida) {
-  Segmento *novoCabeca = (Segmento *)malloc(sizeof(Segmento));
-  novoCabeca->x = cobra->cabeca->x + dx;
-  novoCabeca->y = cobra->cabeca->y + dy;
-  novoCabeca->proximo = cobra->cabeca;
+void adicionarSegmento(Serpente *serpente, int x, int y) {
+  Nodo *novoNodo = (Nodo *)malloc(sizeof(Nodo));
+  novoNodo->posX = x;
+  novoNodo->posY = y;
+  novoNodo->prox = serpente->inicio;
+  serpente->inicio = novoNodo;
+}
 
-  cobra->cabeca = novoCabeca;
+void inicializarSerpente(Serpente *serpente) {
+  serpente->inicio = NULL;
+  serpente->dirX = 1;
+  serpente->dirY = 0;
+  adicionarSegmento(serpente, 10, 10);
+}
 
-  if (novoCabeca->x == comida->x && novoCabeca->y == comida->y) {
-    comeuComida = true;
-    gerarComidaEmLocalAleatorio(comida);
-    contador++;
-  }
-  else {
-    Segmento *atual = cobra->cabeca->proximo;
-    Segmento *segmentoAnterior = cobra->cabeca;
+void criarAlimento(Alimento *alimento) {
+  alimento->posX = rand() % (MAXX - 3) + 2;
+  alimento->posY = rand() % (MAXY - 2) + 2;
+}
 
-    while (atual->proximo != NULL)
-    {
-      segmentoAnterior = atual;
-      atual = atual->proximo;
+void moverSerpente(Serpente *serpente, Alimento *alimento) {
+  int novaPosY = serpente->inicio->posY + serpente->dirY;
+  int novaPosX = serpente->inicio->posX + serpente->dirX;
+
+  adicionarSegmento(serpente, novaPosX, novaPosY);
+
+  if (novaPosX == alimento->posX && novaPosY == alimento->posY) {
+    comeuAlimento = true;
+    criarAlimento(alimento);
+    pontos++;
+  } else {
+    Nodo *atual = serpente->inicio;
+    while (atual->prox->prox != NULL) {
+      atual = atual->prox;
     }
-
-    segmentoAnterior->proximo = NULL;
-    free(atual);
+    free(atual->prox);
+    atual->prox = NULL;
   }
 }
 
-void gerarComidaEmLocalAleatorio(Comida *comida) {
-  comida->x = rand() % (MAXX - 3) + 2;
-  comida->y = rand() % (MAXY - 2) + 2;
-}
-
-void exibirJogo(Cobra *cobra, Comida *comida) {
+void desenharJogo(Serpente *serpente, Alimento *alimento) {
   screenInit(1);
-  printf("  W - Frente   S - Trás   A - Esquerda   D - Direita   ESC - Sair\t Comida: %d", contador);
+  printf("  W-Frente  S-Trás  A-Esquerda  D-Direita  ESC-Sair\t Pontuação: %d", pontos);
 
-  Segmento *segmento = cobra->cabeca;
-  while (segmento != NULL) {
-    screenGotoxy(segmento->x, segmento->y);
-    printf("%s%c%s", corCobra, cobraChar, RESET);
-    segmento = segmento->proximo;
+  Nodo *nodo = serpente->inicio;
+  while (nodo != NULL) {
+    screenGotoxy(nodo->posX, nodo->posY);
+    printf("%s%c%s", corSerpente, charSerpente, RESET);
+    nodo = nodo->prox;
   }
 
-  screenGotoxy(comida->x, comida->y);
-  printf("%s%c%s", corComida, comidaChar, RESET);
+  screenGotoxy(alimento->posX, alimento->posY);
+  printf("%s%c%s", corAlimento, charAlimento, RESET);
 
   screenUpdate();
 }
 
-bool colidiu(Cobra *cobra) {
-  if (cobra->cabeca->x <= 1 || cobra->cabeca->x >= MAXX - 1 || cobra->cabeca->y <= 1 || cobra->cabeca->y >= MAXY - 1) {
-    return true;
-  }
-
-  Segmento *segmento = cobra->cabeca->proximo;
-  while (segmento != NULL) {
-    if (cobra->cabeca->x == segmento->x && cobra->cabeca->y == segmento->y) {
+bool verificarColisao(Serpente *serpente) {
+  Nodo *nodo = serpente->inicio->prox;
+  while (nodo != NULL) {
+    if (serpente->inicio->posX == nodo->posX && serpente->inicio->posY == nodo->posY) {
       return true;
     }
-    segmento = segmento->proximo;
+    nodo = nodo->prox;
   }
-
+  if (serpente->inicio->posX <= 1 || serpente->inicio->posX >= MAXX - 1 || serpente->inicio->posY <= 1 || serpente->inicio->posY >= MAXY - 1) {
+    return true;
+  }
   return false;
 }
 
@@ -285,10 +146,10 @@ void configuracoes() {
   screenInit(0);
   int escolha;
   printf("Configurações do Jogo:\n");
-  printf("1 - Caractere da Comida (Atual: %c)\n", comidaChar);
-  printf("2 - Caractere da Cobra (Atual: %c)\n", cobraChar);
-  printf("3 - Cor da Cobra (Atual: %s)\n", strcmp(corCobra, VERDE) == 0 ? "Verde" : (strcmp(corCobra, AZUL) == 0 ? "Azul" : "Vermelho"));
-  printf("4 - Cor da Comida (Atual: %s)\n", strcmp(corComida, AMARELO) == 0 ? "Amarelo" : (strcmp(corComida, LARANJA) == 0 ? "Laranja" : "Azul"));
+  printf("1 - Caractere da Comida (Atual: %c)\n", charAlimento);
+  printf("2 - Caractere da Serpente (Atual: %c)\n", charSerpente);
+  printf("3 - Cor da Serpente (Atual: %s%c%s)\n", corSerpente, 'O', RESET);
+  printf("4 - Cor da Comida (Atual: %s%c%s)\n", corAlimento, '*', RESET);
   printf("5 - Voltar ao menu principal\n");
 
   while (1) {
@@ -296,40 +157,66 @@ void configuracoes() {
     switch (escolha) {
       case '1':
         printf("Escolha * , $ ou & para a comida: ");
-        comidaChar = readch();
+        charAlimento = readch();
         break;
       case '2':
-        printf("Escolha O, @ ou 0 para a cobra: ");
-        cobraChar = readch();
+        printf("Escolha O, @ ou 0 para a serpente: ");
+        charSerpente = readch();
         break;
       case '3':
-        printf("Escolha a cor da cobra (1 - Verde, 2 - Azul, 3 - Vermelho): ");
+        printf("Escolha a cor da serpente:\n");
+        printf("1 - Preto\n2 - Vermelho\n3 - Verde\n4 - Amarelo\n5 - Azul\n6 - Magenta\n7 - Ciano\n8 - Branco\n9 - Cinza\n0 - Vermelho Claro\n");
         escolha = readch();
-        corCobra = (escolha == '1') ? VERDE : (escolha == '2' ? AZUL : VERMELHO);
+        switch (escolha) {
+          case '1': corSerpente = PRETO; break;
+          case '2': corSerpente = VERMELHO; break;
+          case '3': corSerpente = VERDE; break;
+          case '4': corSerpente = AMARELO; break;
+          case '5': corSerpente = AZUL; break;
+          case '6': corSerpente = MAGENTA; break;
+          case '7': corSerpente = CIANO; break;
+          case '8': corSerpente = BRANCO; break;
+          case '9': corSerpente = CINZA; break;
+          case '0': corSerpente = VERMELHO_CLARO; break;
+        }
         break;
       case '4':
-        printf("Escolha a cor da comida (1 - Amarelo, 2 - Laranja, 3 - Azul): ");
+        printf("Escolha a cor da comida:\n");
+        printf("1 - Preto\n2 - Vermelho\n3 - Verde\n4 - Amarelo\n5 - Azul\n6 - Magenta\n7 - Ciano\n8 - Branco\n9 - Cinza\n0 - Vermelho Claro\n");
         escolha = readch();
-        corComida = (escolha == '1') ? AMARELO : (escolha == '2' ? LARANJA : AZUL);
+        switch (escolha) {
+          case '1': corAlimento = PRETO; break;
+          case '2': corAlimento = VERMELHO; break;
+          case '3': corAlimento = VERDE; break;
+          case '4': corAlimento = AMARELO; break;
+          case '5': corAlimento = AZUL; break;
+          case '6': corAlimento = MAGENTA; break;
+          case '7': corAlimento = CIANO; break;
+          case '8': corAlimento = BRANCO; break;
+          case '9': corAlimento = CINZA; break;
+          case '0': corAlimento = VERMELHO_CLARO; break;
+        }
         break;
       case '5':
         return;
     }
     printf("Configuração atualizada. Pressione qualquer tecla para continuar.\n");
+    readch(); // Wait for any key press before returning to the menu
   }
 }
 
-void salvarPontuacao(const char* nomeJogador, int contador) {
+
+void gravarPontuacao(const char* nomeJogador, int pontuacao) {
   FILE *fp = fopen("ranking.txt", "a");
   if (fp != NULL) {
-    fprintf(fp, "%s %d\n", nomeJogador, contador);
+    fprintf(fp, "%s %d\n", nomeJogador, pontuacao);
     fclose(fp);
   } else {
     printf("Erro ao abrir arquivo de ranking.\n");
   }
 }
 
-void mostrarRanking() {
+void exibirRanking() {
   screenInit(0);
   FILE *fp = fopen("ranking.txt", "r");
   char linha[100];
@@ -345,7 +232,7 @@ void mostrarRanking() {
       if (keyhit()) {
         int tecla = readch();
         if (tecla == 27) {
-          return; // Retornar ao menu principal
+          return;
         }
       }
     }
@@ -356,4 +243,144 @@ void mostrarRanking() {
     }
     readch();
   }
+}
+
+int main() {
+  keyboardInit();
+  screenInit(0);
+  char nomeJogador[50];
+  limparTela();
+  printf("Por favor, insira seu nome: ");
+  fgets(nomeJogador, 50, stdin);
+  nomeJogador[strcspn(nomeJogador, "\n")] = 0;
+
+  limparTela();
+  printf("Bem-vindo, %s!\n", nomeJogador);
+  sleep(2);
+
+  mostrarMenu();
+
+  while (1) {
+    if (keyhit()) {
+      char tecla = readch();
+      switch (tecla) {
+      case '1':
+        velocidade = 150000;
+        comecarJogo = true;
+        break;
+      case '2':
+        velocidade = 100000;
+        comecarJogo = true;
+        break;
+      case '3':
+        velocidade = 80000;
+        comecarJogo = true;
+        break;
+      case '4':
+        velocidade = 50000;
+        comecarJogo = true;
+        break;
+      case '5':
+        velocidade = 30000;
+        comecarJogo = true;
+        break;
+      case '6':
+        velocidade = 20000;
+        comecarJogo = true;
+        break;
+      case '7':
+        configuracoes();
+        mostrarMenu();
+        break;
+      case '8':
+        exibirRanking();
+        mostrarMenu();
+        break;
+      case '9':
+        keyboardDestroy();
+        return 0;
+      }
+    }
+
+    if (comecarJogo) {
+      srand(time(NULL));
+      screenInit(1);
+
+      Alimento alimento;
+      Serpente serpente;
+      criarAlimento(&alimento);
+      inicializarSerpente(&serpente);
+
+      while (comecarJogo) {
+        if (keyhit() && !jogoPerdido) {
+          int tecla = readch();
+          int novo_dx = serpente.dirX;
+          int novo_dy = serpente.dirY;
+
+          switch (tecla) {
+          case 115:
+            novo_dx = 0;
+            novo_dy = 1;
+            break;
+          case 119:
+              novo_dx = 0;
+              novo_dy = -1;
+              break;
+          case 97:
+              novo_dx = -1;
+              novo_dy = 0;
+              break;
+          case 27:
+              comecarJogo = false;
+              gravarPontuacao(nomeJogador, pontos);
+              pontos = 0;
+              mostrarMenu();
+              break;
+          case 100:
+              novo_dx = 1;
+              novo_dy = 0;
+              break;
+          }
+          if (!(novo_dy == -serpente.dirY && novo_dx == -serpente.dirX)) {
+              serpente.dirY = novo_dy;
+              serpente.dirX = novo_dx;
+          }
+        }
+
+        moverSerpente(&serpente, &alimento);
+        desenharJogo(&serpente, &alimento);
+        usleep(velocidade);
+
+        if (verificarColisao(&serpente)) {
+          jogoPerdido = true;
+          screenInit(0);
+          printf("\n\n Você deseja continuar jogando? (s/n)\n");
+          while (1) {
+            if (keyhit()) {
+              char tecla = readch();
+              if (tecla == 's') {
+                inicializarSerpente(&serpente);
+                criarAlimento(&alimento);
+                comeuAlimento = false;
+                jogoPerdido = false;
+                pontos = 0;
+                break;
+              } else if (tecla == 'n') {
+                jogoPerdido = false;
+                comecarJogo = false;
+                gravarPontuacao(nomeJogador, pontos);
+                pontos = 0;
+                mostrarMenu();
+                break;
+              }
+            }
+          }
+        }
+      }
+      screenDestroy();
+      mostrarMenu();
+    }
+  }
+  keyboardDestroy();
+  return 0;
 }
